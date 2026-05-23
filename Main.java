4173ab3693b10f38aa769204ac83d5bf81b98a8d
Debapriya0016 <omm217811@gmail.com>
2026-05-23 11:39:09 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        float b = 5.52f;
+        char c = 'A';
+        boolean d = true;
+        double e = 99.99;
+
+        System.out.println("Integer value: " + a);
+        System.out.println("Float value: " + b);
+        System.out.println("Character value: " + c);
+        System.out.println("Boolean value: " + d);
+        System.out.println("Double value: " + e);
+    }
+}
